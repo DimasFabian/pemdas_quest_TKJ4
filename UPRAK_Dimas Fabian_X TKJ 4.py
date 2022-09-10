40fad22426ee64b00535dd927825f7a5542bdd7b
DimasFabian <112526760+DimasFabian@users.noreply.github.com>
2022-09-10 14:42:44 +0700

    Add files via upload

---
+++ UPRAK_Dimas Fabian_X TKJ 4.py
@@ -0,0 +1,9 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================================================== RESTART: C:/Users/ASUS/Documents/UPRAK_Dimas Fabian_X TKJ 4.py =================================================
+volume kubus adalah 750
+volume kubus adalah 125
+volume limas adaalah 10.0
+volume limas adalah 1540.0
+celcius dijadikan ke reamor 32.0
