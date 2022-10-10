220fda52e0c0548faa6c5710a3a6944b608b727b
DimasFabian <112526760+DimasFabian@users.noreply.github.com>
2022-10-10 10:40:30 +0700

    Add files via upload

---
+++ python10- TKJ4.py
@@ -0,0 +1,23 @@
+# Membuat Tipe data koleksi
+
+koleksi_data_str = ["pisang" , "Mangga" , "Jambu" , "Semangka"]
+
+koleksi_data_int = [200, 300, 400,500]
+
+koleksi_data_mix = ["Rizky Billar", 100, True, "Reza Arap"]
+
+print("Koleksi data Stirng:", koleksi_data_str)
+print("Koleksi data Integer:", koleksi_data_int)
+print("Koleksi data Campuran:", koleksi_data_mix)
+
+# Buatlah 3 kumpulan data: Nama Hewan, NIlai UTS, Nama Teman sebangku kalian
+
+nama_hewan = ["Anjing" , "Babi" , "Beruang"]
+
+nilai_uts = [100, 90, 80]
+
+nama_teman = ["Dimas", "Udin", 100, False]
+
+print("nama hewan", nama_hewan)
+print("nilai uts", nilai_uts)
+print("nama teman", nama_teman)
